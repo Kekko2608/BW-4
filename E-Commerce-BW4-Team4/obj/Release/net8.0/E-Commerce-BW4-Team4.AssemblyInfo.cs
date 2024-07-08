00f0dffce9368ace8ab8ed671397ffacbd2175e3
mariantoniettaangelini <164540376+mariantoniettaangelini@users.noreply.github.com>
2024-07-08 12:37:30 +0200

    txtper popolare tabelle
    .

--- E-Commerce-BW4-Team4/obj/Release/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Release/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9af70b32676031e112c63660dd63945496c23ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
