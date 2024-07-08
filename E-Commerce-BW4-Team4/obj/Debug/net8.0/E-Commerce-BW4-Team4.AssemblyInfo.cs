bc291b97fd7653d1d2f582b9cfdb438ae0e45da4
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-07-08 12:05:38 +0200

    .

--- E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d7aff8b7bb9a33fdf5747ddfeba43ecde3ab73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb58beff2f72da7cfca980318cf1657efa338b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
