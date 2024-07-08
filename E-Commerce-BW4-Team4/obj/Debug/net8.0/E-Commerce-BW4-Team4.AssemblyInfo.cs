75a857d52c13f170dcf1362a94a33a9028f6543f
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-07-08 14:50:49 +0200

    icone e logo + modifiche a gitignore

--- E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f0dffce9368ace8ab8ed671397ffacbd2175e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a737d7047e4aa066fb4b7dc72bd1f8422ef626e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
