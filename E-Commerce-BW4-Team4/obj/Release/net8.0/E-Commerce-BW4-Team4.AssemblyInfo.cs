31eb81a6ccc5c75750f0bc958d91cd90f1e75782
Kekko2608 <francesco.pisu.94@tiscali.it>
2024-07-09 10:09:02 +0200

    .

--- E-Commerce-BW4-Team4/obj/Release/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Release/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c81f721562185586f1b2bdf571515366eb2c342c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6085697b90ea08fe51d1411ac198a21e6afdfde9")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
