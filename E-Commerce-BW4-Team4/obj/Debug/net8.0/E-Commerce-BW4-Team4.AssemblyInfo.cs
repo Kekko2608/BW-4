49f20852526664bbad39f46e087ce3fff33e3e51
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-07-11 15:35:25 +0200

    modifica carrello

--- E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5e76173a4341ebcc1f2ac0d69da95eafb03eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f2c348dfe590e00884f110e7aa43035bac9bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
