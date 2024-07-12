1f067b281973c7d8e3fcd5aac0a90f0477e4d89c
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-07-12 10:17:52 +0200

    ultime modifiche

--- E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
+++ E-Commerce-BW4-Team4/obj/Debug/net8.0/E-Commerce-BW4-Team4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b98e7399c7ce60e69510c28ae28803510875ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f3106846658c6efbf19c5c4665c68d943d9922")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce-BW4-Team4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
